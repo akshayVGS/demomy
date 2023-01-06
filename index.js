dc26690e067a374644c84ca1b768f7c1f257e998
akshay-vigoursoft <122018687+akshayVGS@users.noreply.github.com>
2023-01-06 23:08:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var url = "mongodb+srv://shelke:pMqjIx7qeNdoQyt4@cluster0.8byuiku.mongodb.net/?r
 try {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var dbo = db.db("userss");
-        dbo.collection("userlogins").find().limit(5).toArray(function(err, result) {
+        var dbo = db.db("usera");
+        dbo.collection("users").find().toArray(function(err, result) {
           if (err) throw err;
           console.log(result);
           db.close();
