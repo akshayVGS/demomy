9ef4e51b3e7a5982b7fba4ded5a87bdf4b8d4eef
akshay-vigoursoft <122018687+akshayVGS@users.noreply.github.com>
2023-01-06 20:01:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://akshayuser:<password>@cluster0.bbmvuzv.mongodb.net/";
+
+try {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("userss");
+        dbo.collection("userlogins").find().limit(5).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          db.close();
+        });
+      });
+} catch (error) {
+    console.log(error)
+}
+
+
